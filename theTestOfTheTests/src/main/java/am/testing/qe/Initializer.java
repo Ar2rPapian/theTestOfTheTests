2db793025f557ff9c6779d9ac26e34fdfb9f4e84
hovik777 <hovhannesmkrtchyan40@gmail.com>
2019-10-22 22:48:21 +0400

    fixed minor issues

--- theTestOfTheTests/src/main/java/am/testing/qe/Initializer.java
+++ theTestOfTheTests/src/main/java/am/testing/qe/Initializer.java
@@ -2,6 +2,8 @@ package am.testing.qe;
 
 import am.testing.qe.util.Browser;
 
+import static am.testing.qe.config.Constants.DEFAULT_BROWSER;
+
 public class Initializer {
 
     private static Initializer initializer = new Initializer();
@@ -13,7 +15,11 @@ public class Initializer {
     }
 
     public Browser getCurrentBrowser() {
-        String strBrowserName = System.getenv("browser").toUpperCase();
+        String browserName = System.getenv("browser");
+        if(browserName == null){
+            return DEFAULT_BROWSER;
+        }
+        String strBrowserName = browserName.toUpperCase();
         return Browser.valueOf(strBrowserName);
     }
 }
--- theTestOfTheTests/src/main/java/am/testing/qe/config/Constants.java
+++ theTestOfTheTests/src/main/java/am/testing/qe/config/Constants.java
@@ -1,5 +1,8 @@
 package am.testing.qe.config;
 
+import am.testing.qe.util.Browser;
+
 public class Constants {
     public static final int DEFAULT_TIMEOUT_4_EVERYTHING = 30000;
+    public static final Browser DEFAULT_BROWSER = Browser.CHROME;
 }
